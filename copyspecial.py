278e6e63e8ea87424cb50e484c645a04905f5232
Kathryn Anderson <kat42272@yahoo.com>
2020-06-23 16:50:50 -0700

    Update Gather and Zip functions

--- copyspecial.py
+++ copyspecial.py
@@ -7,7 +7,8 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 # give credits
-__author__ = "Amanda Yonce, Nikal Morgan, and study group, written by Kathryn Anderson"
+__author__ = """Amanda Yonce, Nikal Morgan, Daniel Lomelino,
+            and study group, written by Kathryn Anderson"""
 
 import re
 import os
@@ -19,12 +20,14 @@ import argparse
 
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
-    special_dir_files = []
-    for file in os.listdir(dirname):
-        special_file = re.findall(r'__(\w+)__', file)
-        if special_file:
-            special_dir_files.append(os.path.abspath(file))
-    return special_dir_files
+    paths = os.listdir(dirname)
+    special_paths = []
+    for filename in paths:
+        match = re.search(r'__\w+__', filename)
+        if match:
+            special_paths.append(os.path.abspath(
+                os.path.join(dirname, filename)))
+    return special_paths
 
 
 def copy_to(path_list, dest_dir):
@@ -36,10 +39,12 @@ def copy_to(path_list, dest_dir):
 
 
 def zip_to(path_list, dest_zip):
-    for path in path_list:
-        print(f'zip -j {dest_zip} {path}')
-        subprocess.run(['zip', '-j', dest_zip, path])
-    return
+    """Zip Special Files"""
+    cmd = ['zip', '-j', dest_zip]
+    cmd.extend(path_list)
+    print("Command I'm going to do:")
+    print(' '.join(cmd))
+    subprocess.check_output(cmd)
 
 
 def main(args):
@@ -48,7 +53,6 @@ def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
-    # TODO: add one more argument definition to parse the 'from_dir' argument
     parser.add_argument('from_dir', help='find dir for special files')
     ns = parser.parse_args(args)
 
@@ -61,20 +65,23 @@ def main(args):
     # exit(1).
 
     # Your code here: Invoke (call) your functions
+    special_paths = get_special_paths(ns.from_dir)
 
-
-    if len(sys.argv) < 1:
-        parser.print_usage()
-    path_list = get_special_paths(ns.from_dir)
-    if ns.todir:
-        copy_to(path_list, ns.todir)
-    elif ns.tozip:
-        zip_to(path_list, ns.tozip)
+    if ns.tozip:
+        zip_to(special_paths, ns.tozip)
     else:
-        print(*path_list, sep='\n')
-        # for path in path_list:
-        #     print(path)
-
+        print('\n'.join(special_paths))
+    # if len(sys.argv) < 1:
+    #     parser.print_usage()
+    # path_list = get_special_paths(ns.from_dir)
+    # if ns.todir:
+    #     copy_to(path_list, ns.todir)
+    # elif ns.tozip:
+    #     zip_to(path_list, ns.tozip)
+    # else:
+    #     print(*path_list, sep='\n')
+    # for path in path_list:
+    #     print(path)
 
 
 if __name__ == "__main__":
